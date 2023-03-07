cec4c1aa28c5fc7a16e153e530a5da9b162cbc0d
Okoro Nnamdi <okoronnamdi4044@gmail.com>
2023-03-07 23:30:23 +0100

    ProductByCategory

---
+++ CmsShoppingChart/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using CmsShoppingChart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingChart.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly CmsShoppingContext _context;
+		public ProductsController(CmsShoppingContext context)
+		{
+			_context = context;
+		}
+        //Get/Product
+		public async Task<IActionResult> Index(int p =1)
+		{
+			var pageSize = 6;
+
+			var productlist = _context.products.OrderByDescending(x => x.Id)
+															.Skip((p - 1) * pageSize)
+															.Take(pageSize);
+															
+			ViewBag.PageNumber = p;
+			ViewBag.PageRange = pageSize;
+			ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.products.Count() / pageSize);
+			return View(await productlist.ToListAsync());
+		}
+	}
+}
